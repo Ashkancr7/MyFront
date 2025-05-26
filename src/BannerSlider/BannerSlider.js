4562a29cef59d8894ab50802ca630d2846b58b9f
unknown <ashkan.eng.asadi@gmail.com>
2025-05-26 15:22:03 +0330

    dispیب

--- src/BannerSlider/BannerSlider.js
+++ src/BannerSlider/BannerSlider.js
@@ -16,7 +16,7 @@ const banners = [
 
 const BannerSlider = () => {
   return (
-    <div className="mx-auto mt-5 overflow-x-hidden max-w-screen">
+    <div className="mx-auto mt-5 overflow-x-hidden w-full max-w-full">
       <Swiper
         spaceBetween={10}
         slidesPerView={1}
@@ -24,16 +24,21 @@ const BannerSlider = () => {
         autoplay={{ delay: 3000 }}
         pagination={{ clickable: true }}
         modules={[Pagination, Autoplay]}
-        className="rounded-lg shadow-2xl"
+        className="rounded-lg shadow-2xl w-full"
       >
         {banners.map((banner) => (
-          <SwiperSlide onClick={()=> console.log(banner.id)} key={banner.id}>
-            <img src={banner.image} alt={`Banner ${banner.id}`} className="w-full  object-contain rounded-lg cursor-pointer" />
+          <SwiperSlide onClick={() => console.log(banner.id)} key={banner.id}>
+            <img
+              src={banner.image}
+              alt={`Banner ${banner.id}`}
+              className="w-full h-48 sm:h-64 md:h-80 object-cover rounded-lg cursor-pointer"
+            />
           </SwiperSlide>
         ))}
       </Swiper>
     </div>
   );
 };
+
 
 export default BannerSlider;
