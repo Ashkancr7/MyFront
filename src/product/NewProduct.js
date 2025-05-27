ec70221784ebf8d37a65965ef38904bd84f34aa2
unknown <ashkan.eng.asadi@gmail.com>
2025-05-27 11:53:14 +0330

    Adلتنلن

--- src/product/NewProduct.js
+++ src/product/NewProduct.js
@@ -150,32 +150,41 @@ const NewProduct = () => {
     return (
 
         <div className="w-full overflow-x-auto px-2 custom-scrollbar">
-        <div className="flex gap-4 w-max">
-          {products.map((product) => (
-            <div
-              key={product.id}
-              className="w-[calc(20%-8px)] sm:w-[calc(25%-10px)] md:w-[calc(20%-12px)] lg:w-[calc(16.666%-12px)]"
-            >
-              <DiscuntProductCard product={{ ...product, price: Number(product.price) }} />
+            <div className='flex flex-row align-center justify-center'>
+
+                <hr class="w-80 h-0.5 mt-5 ml-3 bg-black border-0 rounded-sm dark:bg-gray-700"></hr>
+
+                <h2 className="text-sm  sm:text-base md:text-2xl font-semibold mb-6 text-center">محصولات بچگانه</h2>
+
+                <hr class="w-80 h-0.5 mt-5 mr-3 bg-black border-0 rounded-sm dark:bg-gray-700"></hr>
+
+            </div >
+            <div className="flex gap-4 w-max">
+                {products.map((product) => (
+                    <div
+                        key={product.id}
+                        className="w-[calc(20%-8px)] sm:w-[calc(25%-10px)] md:w-[calc(20%-12px)] lg:w-[calc(16.666%-12px)]"
+                    >
+                        <DiscuntProductCard product={{ ...product, price: Number(product.price) }} />
+                    </div>
+                ))}
+
+                {/* دکمه مشاهده همه */}
+                <div className="w-[calc(33.333%-8px)] sm:w-[calc(25%-10px)] md:w-[calc(20%-12px)] lg:w-[calc(16.666%-12px)] flex items-center justify-center">
+                    <button
+                        onClick={() => navigate('/DiscountProduct')}
+                        className="w-full h-full bg-white rounded shadow flex flex-col items-center justify-center hover:bg-gray-200 p-4"
+                    >
+                        <span>مشاهده همه</span>
+                        <svg className="w-4 h-4 mt-2 text-black" viewBox="0 0 8 14" fill="none">
+                            <path d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                        </svg>
+                    </button>
+                </div>
             </div>
-          ))}
-      
-          {/* دکمه مشاهده همه */}
-          <div className="w-[calc(33.333%-8px)] sm:w-[calc(25%-10px)] md:w-[calc(20%-12px)] lg:w-[calc(16.666%-12px)] flex items-center justify-center">
-            <button
-              onClick={() => navigate('/DiscountProduct')}
-              className="w-full h-full bg-white rounded shadow flex flex-col items-center justify-center hover:bg-gray-200 p-4"
-            >
-              <span>مشاهده همه</span>
-              <svg className="w-4 h-4 mt-2 text-black" viewBox="0 0 8 14" fill="none">
-                <path d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-              </svg>
-            </button>
-          </div>
         </div>
-      </div>
-      
-      
+
+
 
 
     );
