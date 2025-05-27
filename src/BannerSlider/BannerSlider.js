705ddf94277c26e6d73719d1e7687eab9964d7a5
unknown <ashkan.eng.asadi@gmail.com>
2025-05-27 14:23:21 +0330

    Aghhj

--- src/BannerSlider/BannerSlider.js
+++ src/BannerSlider/BannerSlider.js
@@ -1,42 +1,70 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import { Pagination, Autoplay } from "swiper/modules";
-import bann from '../assets/banner.jpg'
+import { Pagination, Autoplay, Navigation } from "swiper/modules";
+import { ChevronLeft, ChevronRight } from "lucide-react"; // آیکون‌ها
+import banner1 from "../assets/banner.jpg";
+import banner2 from "../assets/banner.jpg";
+import banner3 from "../assets/banner.jpg";
 
 const banners = [
-  { id: 1, image: bann },
-  { id: 2, image: bann },
-  { id: 3, image: bann },
-  { id: 4, image: bann },
-  { id: 5, image: bann },
-  { id: 6, image: bann },
+  { id: 1, image: banner1 },
+  { id: 2, image: banner2 },
+  { id: 3, image: banner3 },
 ];
 
 const BannerSlider = () => {
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
+
   return (
     <div className="w-full max-w-screen-xl mx-auto mt-5 px-2">
-    <Swiper
-      spaceBetween={10}
-      slidesPerView={1}
-      loop={true}
-      autoplay={{ delay: 3000 }}
-      pagination={{ clickable: true }}
-      modules={[Pagination, Autoplay]}
-      className="rounded-lg shadow-2xl"
-    >
-      {banners.map((banner) => (
-        <SwiperSlide key={banner.id} onClick={() => console.log(banner.id)}>
-          <img
-            src={banner.image}
-            alt={`Banner ${banner.id}`}
-            className="w-full h-48 sm:h-64 md:h-80 object-cover rounded-lg cursor-pointer"
-          />
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  </div>
+      <div className="relative rounded-2xl overflow-hidden shadow-lg">
+        {/* دکمه‌های ناوبری */}
+        <button
+          ref={prevRef}
+          className="absolute top-1/2 left-3 z-10 -translate-y-1/2 bg-black/50 text-white p-2 rounded-full hover:bg-black/70 transition"
+        >
+          <ChevronLeft size={20} />
+        </button>
+        <button
+          ref={nextRef}
+          className="absolute top-1/2 right-3 z-10 -translate-y-1/2 bg-black/50 text-white p-2 rounded-full hover:bg-black/70 transition"
+        >
+          <ChevronRight size={20} />
+        </button>
+
+        <Swiper
+          spaceBetween={10}
+          slidesPerView={1}
+          loop={true}
+          autoplay={{ delay: 3000 }}
+          pagination={{ clickable: true }}
+          navigation={{
+            prevEl: prevRef.current,
+            nextEl: nextRef.current,
+          }}
+          onInit={(swiper) => {
+            swiper.params.navigation.prevEl = prevRef.current;
+            swiper.params.navigation.nextEl = nextRef.current;
+            swiper.navigation.init();
+            swiper.navigation.update();
+          }}
+          modules={[Pagination, Autoplay, Navigation]}
+        >
+          {banners.map((banner) => (
+            <SwiperSlide key={banner.id}>
+              <img
+                src={banner.image}
+                alt={`Banner ${banner.id}`}
+                className="w-full h-48 sm:h-64 md:h-80 lg:h-[400px] xl:h-[450px] object-cover cursor-pointer transition-transform duration-300 hover:scale-105"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
   );
 };
 
