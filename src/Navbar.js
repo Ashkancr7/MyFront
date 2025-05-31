7b8bcf180014bdea3f3ffb7d8bc46460691bfb31
unknown <ashkan.eng.asadi@gmail.com>
2025-05-31 17:39:09 +0330

    edit sign

--- src/Navbar.js
+++ src/Navbar.js
@@ -112,25 +112,28 @@ const Navbar = () => {
                 </span>
               </button>
 
-              {user ? (
-                <>
-                  <span className="text-sm text-gray-700">{user.nam + " " + user.lname || "کاربر"}</span>
-                  <button
-                    onClick={logout}
-                    className="px-3 py-2 border border-red-400 rounded-md hover:bg-red-100 text-red-600 text-sm"
-                  >
-                    خروج از حساب
-                  </button>
+              <button
+                onClick={() => {user ? logout() :navigate('/login')} }
+                className="flex items-center  border border-gray-300 rounded-md hover:bg-gray-100"
+              >
+                {
+                  user ? (
+                   <>
+                  <span className="block px-3 py-2 text-sm text-gray-700">{user.nam + " " + user.lname || "کاربر"}</span>
+                 
                 </>
-              ) : (
-                <button
-                  onClick={() => navigate('/login')}
-                  className="flex items-center space-x-1 px-3 py-2 border border-gray-300 rounded-md hover:bg-gray-100"
-                >
-                  <FaUser size={14} />
-                  <span>ورود / ثبت‌نام</span>
-                </button>
-              )}
+                  ) : (
+                    <>
+                      <FaUser size={14} />
+                      <span>ورود / ثبت‌نام</span>
+                    </>
+                  )
+                }
+
+              </button
+
+              >
+
             </div>
           </div>
         </div>
